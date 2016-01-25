6c081bcff8c8e2076d198f87e436918de4670a81
rafeh01 <rafehqazi1@gmail.com>
2016-01-24 19:13:52 -0600

    added the functionality where it returns food type based on ingredients.

---
+++ food.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class food {
+    public static void main(String[] args) { 
+        Map<String, String> map = new HashMap<String, String>();
+        // map.put(["riccotta cheese", "large flat noodles", "parmesan", "spaghetti"], "Lasagna");
+        // System.out.println(map.get(["riccotta cheese", "large flat noodles", "parmesan", "spaghetti"]));
+        map.put("toast eggs milk cinnamon vanilla extract sugar","French Toast");
+        System.out.println(map.get("toast eggs milk cinnamon vanilla extract sugar"));
+        map.put("ground beef onion green pepper eggs cumin salt pepper","Meatloaf");
+        System.out.println(map.get("ground beef onion green pepper eggs cumin salt pepper"));
+    }
+}
+        
+        
+        
+// riccotta cheese
+// large flat noodles
+// parmesan
+// spaghetti sauce
+// 
+// Lasagna
+
+
+
+
